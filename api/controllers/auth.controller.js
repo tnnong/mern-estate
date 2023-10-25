8b979672c66238a403e8117f3ef3f992f2d241d2
DESKTOP-MG66VR2\PC <nongtranngoc@gmail.com>
2023-10-25 15:00:53 +0700

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export const signin = async(req, res, next) => {
     try {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'User not found'));
-        const validPassword = await User.findOne({ password });
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Invalid password'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
